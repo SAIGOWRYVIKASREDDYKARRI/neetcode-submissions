d8de6db0050c0d399e12dbff817d633feaaec3cc
Karri Sai Gowry Vikas Reddy <vikaskarri456@gmail.com>
2026-05-22 11:21:17 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size()==0)
+        {
+            return true;
+        }
+        intervals.sort((a,b)->Integer.compare(a.start,b.start));
+        int prev_end=intervals.get(0).end;
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(intervals.get(i).start<prev_end)
+            {
+                return false;
+            }
+            else
+            {
+                prev_end=intervals.get(i).end;
+            }
+        }
+        return true;
+    }
+}
